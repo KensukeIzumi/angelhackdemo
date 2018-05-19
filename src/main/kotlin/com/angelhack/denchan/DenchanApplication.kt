e45c04eec20bd4744113d96185e48b7d824fac85
KensukeIzumi <kensuke.izumi.i@finc.com>
2018-05-20 00:54:04 +0900

    initial commit

---
+++ src/main/kotlin/com/angelhack/denchan/DenchanApplication.kt
@@ -0,0 +1,32 @@
+package com.angelhack.denchan
+
+import com.linecorp.bot.model.event.Event
+import com.linecorp.bot.model.event.MessageEvent
+import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.TextMessage
+import com.linecorp.bot.spring.boot.annotation.EventMapping
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@LineMessageHandler
+class DenchanApplication {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<DenchanApplication>(*args)
+        }
+
+        @EventMapping
+        fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>): TextMessage =
+                TextMessage(event.message.text)
+
+        @EventMapping
+        fun handleDefaultMessageEvent(event: Event) {
+            System.out.print(event)
+        }
+    }
+}
+
